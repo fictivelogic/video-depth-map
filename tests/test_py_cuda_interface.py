29a7f3070aae4ea8c13979536772e3ec29b3854a
Samuel Richerd <sricherd@caltech.edu>
2015-05-30 18:10:48 -0700

    Create basic Py-CUDA test
    Note that max number of blocks= 1024

--- tests/test_py_cuda_interface.py
+++ tests/test_py_cuda_interface.py
@@ -1,6 +1,30 @@
 
 
 def test_python_can_call_cuda_kernel():
-    import pycuda
-    pass
+    import pycuda.autoinit
+    import pycuda.driver as drv
+    import numpy as np
+    from pycuda.compiler import SourceModule
+    mod = SourceModule("""
+    __global__ void multiply_them(float *dest, float *a, float *b)
+    {
+        const int i = threadIdx.x;
+        dest[i] = a[i] * b[i];
+    }
+    """)
+
+    multiply_them = mod.get_function('multiply_them')
+
+    test_size = 400
+    a = np.random.randn(test_size).astype(np.float32)
+    b = np.random.randn(test_size).astype(np.float32)
+
+    dest = np.zeros_like(a)
+    multiply_them(drv.Out(dest), drv.In(a), drv.In(b),
+                  block=(test_size, 1, 1), grid=(1, 1))
+
+    expected_product = np.array(a * b, dtype=np.float32)
+    np.testing.assert_array_equal(dest, expected_product)
+
+
 
