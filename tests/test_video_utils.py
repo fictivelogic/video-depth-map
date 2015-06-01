a551a75c82e1193b250fd9efd2b7bc42c46f6468
Samuel Richerd <sricherd@caltech.edu>
2015-05-31 23:44:16 -0700

    Rename video_utils.py

--- tests/test_video_utils.py
+++ tests/test_video_utils.py
@@ -2,7 +2,7 @@ from os.path import join, abspath
 
 
 def test_load_video_from_file_returns_correct_type():
-    from utils.video_interface import load_video_from_file_name
+    from utils.video_utils import load_video_from_file_name
     from cv2 import VideoCapture
     filename = 'Mukuge.avi'
     video_file = load_video_from_file_name(filename=filename)
@@ -11,13 +11,15 @@ def test_load_video_from_file_returns_correct_type():
 
 
 def test_get_next_still_image_from_video_returns_correct_type():
-    from utils.video_interface import load_video_from_file_name
-    from utils.video_interface import get_still_frame_from_video_capture
+    from utils.video_utils import load_video_from_file_name
+    from utils.video_utils import get_still_frame_from_video_capture
     from cv2 import VideoCapture
     import numpy as np
     filename = 'Mukuge.avi'
     video_object = load_video_from_file_name(filename=filename)
-    return_status, still_frame = get_still_frame_from_video_capture(video_capture=video_object)
+    return_status, still_frame = get_still_frame_from_video_capture(
+        video_capture=video_object
+    )
     assert return_status == True
     assert isinstance(still_frame,  np.ndarray)
 
