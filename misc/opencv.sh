9af79c9c6fbb672bb0886ed3bd1b228edf9a7ac7
Samuel Richerd <sricherd@caltech.edu>
2015-06-07 00:59:59 -0700

    Use opencv-contrib as part of OpenCV build
    This involved making a git clone of the opencv-contrib repo
    available at https://github.com/Itseez/opencv-contrib.git

--- misc/opencv.sh
+++ misc/opencv.sh
@@ -1,5 +1,5 @@
 #version="$(wget -q -O - http://sourceforge.net/projects/opencvlibrary/files/opencv-unix | egrep -m1 -o '\"[0-9](\.[0-9]+)+' | cut -c2-)"
-version="3.0.0-rc1"
+version="3.0.0"
 echo "Installing OpenCV" $version
 mkdir OpenCV
 cd OpenCV
@@ -14,8 +14,8 @@ unzip OpenCV-$version.zip
 cd opencv-$version
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
-make -j2
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules ..
+make -j4
 sudo checkinstall
 sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
 sudo ldconfig
