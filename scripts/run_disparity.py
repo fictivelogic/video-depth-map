6b39a3db14a5974df5c0fc0f36fcda9c25d98e1a
obinnae <binnixe@gmail.com>
2015-06-07 05:54:51 -0400

    now streaming video

--- scripts/run_disparity.py
+++ scripts/run_disparity.py
@@ -24,23 +24,30 @@ if __name__=='__main__':
     left_img, right_img = split_stereo_frame_into_left_and_right_frames(
         stereo_frame=stereo_image
     )
-    print('Computing disparity on GPU...')
-    disparity_img = cuda_compute_disparity(
-        image_left=left_img,
-        image_right=right_img,
-        foreground_left=np.ones(shape=(left_img.shape[0:1]),
-                                dtype=np.uint8),
-        foreground_right=np.ones(shape=(left_img.shape[0:1]),
-                                 dtype=np.uint8),
-        window_size=20,
-        block_shape=(512, 1, 1),
-        grid_shape=(math.ceil(left_img.shape[0]* left_img.shape[1]/512), 1, 1)
-    )
-    print('Got : ')
-    print(disparity_img)
-    print(np.amax(disparity_img))
-    cv2.imshow('Image:', left_img)
-    cv2.imshow('Disparity:', 5 * disparity_img.astype(np.uint8))
-    cv2.waitKey(0)
+
+    while success:
+        print('splitting next frame')
+        left_img, right_img = split_stereo_frame_into_left_and_right_frames(
+            stereo_frame=stereo_image
+        )
+        print('Computing disparity on GPU...')
+        disparity_img = cuda_compute_disparity(
+            image_left=left_img,
+            image_right=right_img,
+            foreground_left=np.ones(shape=(left_img.shape[0:1]),
+                                    dtype=np.uint8),
+            foreground_right=np.ones(shape=(left_img.shape[0:1]),
+                                     dtype=np.uint8),
+            window_size=20,
+            block_shape=(512, 1, 1),
+            grid_shape=(math.ceil(left_img.shape[0]* left_img.shape[1]/512), 1, 1)
+        )
+        print('Got : ')
+        print(disparity_img)
+        print(np.amax(disparity_img))
+        cv2.imshow('Image:', left_img)
+        cv2.imshow('Disparity:', 5 * disparity_img.astype(np.uint8))
+        cv2.waitKey(1000)
+        success, stereo_image = get_stereo_frame_from_video_capture(video_obj)
     video_obj.release()
 
